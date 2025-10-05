74c745a3261a6db5473066688e25b14101c21326
Charles <wanchuan21@gmail.com>
2025-10-05 14:32:44 +0800

    feat: homepage reset connection

--- src/handle_request.js
+++ src/handle_request.js
@@ -8,10 +8,7 @@ export async function handleRequest(request) {
   const search = url.search;
 
   if (pathname === '/' || pathname === '/index.html') {
-    return new Response('Proxy is Running!  More Details: https://github.com/tech-shrimp/gemini-balance-lite', {
-      status: 200,
-      headers: { 'Content-Type': 'text/html' }
-    });
+    return Response.error();
   }
 
   if (pathname === '/verify' && request.method === 'POST') {
